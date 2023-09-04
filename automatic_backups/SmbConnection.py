1d7385633cb9dbec41d6b2b1dce4c9b3c3d8b860
Cruzeta <tiagocruzeta2005@hotmail.com>
2023-09-04 11:28:47 +0100

    commit

--- automatic_backups/SmbConnection.py
+++ automatic_backups/SmbConnection.py
@@ -1,5 +1,14 @@
 import smbclient
+import os
 
 class SmbConnection:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, server=os.environ['SERVER'] , workgroup=os.environ['WORKGROUP'] , user=os.environ['SERVERUSER'] , password=os.environ['SERVERPASSWORD']) -> None:
+        
+        self.user = os.environ['SERVERUSER'] 
+        self.password = os.environ['SERVERPASSWORD'] 
+        self.workgroup = os.environ['WORKGROUP'] 
+        self.smb = smbclient
+        
+    def start_connection(self):
+        
+        self.smb.register_session()
